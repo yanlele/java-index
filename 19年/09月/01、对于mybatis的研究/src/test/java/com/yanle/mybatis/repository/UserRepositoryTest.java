d655bdae5730cb855c01eb5b9b0e349917cbba1b
yanlele <331393627@qq.com>
2019-09-02 23:38:35 +0800

    [+]添加最基本的正删改查

--- 19年/09月/01、对于mybatis的研究/src/test/java/com/yanle/mybatis/repository/UserRepositoryTest.java
+++ 19年/09月/01、对于mybatis的研究/src/test/java/com/yanle/mybatis/repository/UserRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -25,5 +27,27 @@ public class UserRepositoryTest {
         System.out.println(user);
     }
 
+    @Test
+    public void insertUser() throws Exception {
+        User user = new User();
+        user.setAge(16);
+        user.setBirthday(new Date("1900/12/12"));
+        user.setName("孙中山");
+        user.setPassword("123456");
+        user.setSex(1);
+        user.setUserName("five");
+        this.userRepository.insertUser(user);
+    }
 
+    @Test
+    public void updateUser() throws Exception {
+        User user = userRepository.queryUserById(3);
+        user.setUserName("王大雕");
+        this.userRepository.updateUser(user);
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        userRepository.deleteUser("3");
+    }
 }
