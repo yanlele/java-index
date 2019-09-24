059c418389509919a146cab2c12281af9f8dd9c1
yanlele <331393627@qq.com>
2019-09-24 23:48:50 +0800

    [+]FindMinimalK

---
+++ 19年/10月/03、demo/FindMinimalK.java
@@ -0,0 +1,15 @@
+/**
+ * 第四题
+ * 程序功能：已知，问最大为什么值能够使得?  (变种，最小的K，使得sum(k)>2000)
+ */
+public class FindMinimalK {
+    public static void main(String[] args) {
+        int sum = 1;
+        int k = 1;
+        while (sum <= 2000) {
+            k++;
+            sum = sum + k;
+        }
+        System.out.println("the max k=" + k);
+    }
+}
