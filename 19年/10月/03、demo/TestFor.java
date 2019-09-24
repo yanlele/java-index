df1dcb2a86de0db6fb5a08dd821653886fda3673
yanlele <331393627@qq.com>
2019-09-24 23:46:21 +0800

    [+]TestFor

---
+++ 19年/10月/03、demo/TestFor.java
@@ -0,0 +1,14 @@
+/**
+ * 第三题
+ * for 循环语句练习
+ * 程序功能：将1至100之间所有的整数求和。
+ */
+public class TestFor {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
