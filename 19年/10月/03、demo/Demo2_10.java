a30f93b2911a08a511c58da8cf1244de2deaf578
yanlele <331393627@qq.com>
2019-09-24 23:52:07 +0800

    [+]Demo2_10

---
+++ 19年/10月/03、demo/Demo2_10.java
@@ -0,0 +1,14 @@
+/**
+ * 第五题
+ * 程序功能：求1＋2＋…+100 之和，并将求和表达式与所求的和显示出来。
+ */
+public class Demo2_10 {
+    public static void main(String[] args) {
+        int n = 1, sum = 0;
+        do {
+            sum += n;
+            n++;
+        } while (n <=100);
+        System.out.println("1+2＋...+100 ="+sum);
+    }
+}
--- 19年/10月/03、demo/TestFor.java
+++ 19年/10月/03、demo/TestFor.java
@@ -6,7 +6,7 @@
 public class TestFor {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 101; i++) {
             sum += i;
         }
         System.out.println(sum);
