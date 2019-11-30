1682b818dc877d3e90b82ac4e6729f4afb883765
yanlele <331393627@qq.com>
2019-11-30 17:27:45 +0800

    feat: modify

---
+++ 19年/11月/demo10/Student.java
@@ -0,0 +1,60 @@
+package demo10;
+
+public class Student {
+    private String name;
+    private int number;
+    private int age;
+    private String major;
+
+    public Student() {
+    }
+
+    public Student(String name, int number, int age, String major) {
+        this.name = name;
+        this.number = number;
+        this.age = age;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                ", age=" + age +
+                ", major='" + major + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
